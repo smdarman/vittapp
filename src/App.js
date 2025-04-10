d13a02c1c66e7828ea4e0025142c29f8bdfad48b
Salaiman Darman <salaimandarman@MacBook-Air.local>
2025-04-10 14:59:14 +1000

    new changes:

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
